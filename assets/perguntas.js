f350a4c478c2617f71da6a732dfb5d32ac8bfec5
ROBERTO.ZX <126821263+BLACKWHITY444@users.noreply.github.com>
2025-08-05 16:27:05 -0300

    05/08/2025
     js app com function

---
+++ assets/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Matemática',
+    'Qual é o dígito seguinte? 1,4,8,12,_?',
+    '16'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual lugar do mundo esta localizada a torre Eiffel',
+    'Paris'
+)
+
+criaCartao(
+    'Games',
+    'Qual é o jogo de mundo aberto mais jogado e conhecido no mundo?',
+    'Minecraft'
+)
